91372f210e49fc644a72fe2664542e8d58a94a8e
binlyz <binlyzhuo@outlook.com>
2019-12-02 16:17:00 +0800

    ~

--- VueJsProject/Controllers/VueController.cs
+++ VueJsProject/Controllers/VueController.cs
@@ -19,5 +19,10 @@ namespace VueJsProject.Controllers
         {
             return View();
         }
+
+        public ActionResult Index3()
+        {
+            return View();
+        }
     }
 }
